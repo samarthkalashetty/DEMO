47a81d27a52f7ab58003dddd716b8f1df1ee15dd
samarthkalashetty <samarthkalashetti2612@gmail.com>
2023-07-08 14:35:51 +0530

    multiplication of number

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace DEMO
+﻿
+namespace DEMO
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("enter first number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter third number");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int mul = a * b * c;
+            Console.WriteLine("Multiplication of Three numbers" + mul);
         }
     }
 }
+    
